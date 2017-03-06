cefd1826a67eb5361319118311bfa4cf148198ff
ThiagoLopes <thiagolopes@protonmail.com>
2017-03-06 11:19:27 -0300

    fix shell script if string, add mkdir undodir

--- install-vim.sh
+++ install-vim.sh
@@ -8,13 +8,13 @@ source ~/.vimconfigs/vimrcs/basics_plugin.vim
 source ~/.vimconfigs/vimrcs/extendeds_plugin.vim
 source ~/.vimconfigs/vimrcs/colorschemes.vim" > ~/.vimrc
 
-read -p 'Do you want to enable Python support? (Yn)' python_support
-read -p 'Do you want to enable Javascript support? (Yn)' js_support
-read -p 'Do you want to enable frontend support? (Yn)' frontend_support
-read -p 'Do you want to enable autocomplete support? (Yn)' autocomplete_support
-read -p 'Do you want to enable beta features? (Yn)' beta_support
+read -p 'Do you want to enable Python support? (y/n): ' python_support
+read -p 'Do you want to enable Javascript support? (y/n): ' js_support
+read -p 'Do you want to enable frontend support? (y/n): ' frontend_support
+read -p 'Do you want to enable autocomplete support? (y/n): ' autocomplete_support
+read -p 'Do you want to enable beta features? (y/n): ' beta_support
 
-if [ $python_support == 'Y' ]
+if [ $python_support = "y" ]
 then
     echo 'source ~/.vimconfigs/vimrcs/python.vim' >> ~/.vimrc
     echo 'Python support enabled.'
@@ -22,7 +22,7 @@ else
     echo 'Python support disabled.'
 fi
 
-if [ $js_support == 'Y' ]
+if [ $js_support = "y" ]
 then
     echo 'source ~/.vimconfigs/vimrcs/js.vim' >> ~/.vimrc
     echo 'Javascript support enabled.'
@@ -30,7 +30,7 @@ else
     echo 'Javascript support disabled.'
 fi
 
-if [ $frontend_support == 'Y' ]
+if [ $frontend_support = "y" ]
 then
     echo 'source ~/.vimconfigs/vimrcs/frontend.vim' >> ~/.vimrc
     echo 'Frontend support enabled.'
@@ -38,7 +38,7 @@ else
     echo 'Frontend support disabled.'
 fi
 
-if [ $autocomplete_support == 'Y' ]
+if [ $autocomplete_support = "y" ]
 then
     echo 'source ~/.vimconfigs/vimrcs/autocomplete.vim' >> ~/.vimrc
     echo 'Autocomplete support enabled.'
@@ -46,7 +46,7 @@ else
     echo 'Autocomplete support disabled.'
 fi
 
-if [ $beta_support == 'Y' ]
+if [ $beta_support = "y" ]
 then
     echo 'source ~/.vimconfigs/vimrcs/testing.vim' >> ~/.vimrc
     echo 'Beta support enabled.'
@@ -60,6 +60,9 @@ source ~/.vimconfigs/my_configs.vim
 catch
 endtry
 source ~/.vimconfigs/vimrcs/colors.vim' >> ~/.vimrc
+
+# create undodir
+mkdir undodir
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
 
