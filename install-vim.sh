3f2d2f2ca0b9efba39f91cdd1193d920691ab9cf
ThiagoLopes <thiagolopes@protonmail.com>
2017-03-07 19:49:18 -0300

    update install-vim for folder plugins and basic

--- install-vim.sh
+++ install-vim.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
-echo "set runtimepath+=~/.vimconfigs
-source ~/.vimconfigs/vimrcs/basics.vim
-source ~/.vimconfigs/vimrcs/extendeds.vim
+echo '
+" set main path
+set runtimepath+=~/.vimconfigs
+
+" symbolic link basic config (without plugins)
+source ~/.vimconfigs/vimrcs/basics/basics.vim
+source ~/.vimconfigs/vimrcs/basics/extendeds.vim
+
+" symbolic link for vim-plug
 source ~/.vimconfigs/vimrcs/plug.vim
-call plug#begin('~/.vimconfigs/plugged/')
-source ~/.vimconfigs/vimrcs/basics_plugin.vim
-source ~/.vimconfigs/vimrcs/extendeds_plugin.vim
-source ~/.vimconfigs/vimrcs/colorschemes.vim" > ~/.vimrc
+
+" symbolic link for plugins
+call plug#begin("~/.vimconfigs/plugged/")
+
+source ~/.vimconfigs/vimrcs/plugins/basics_plugin.vim
+source ~/.vimconfigs/vimrcs/plugins/extendeds_plugin.vim
+source ~/.vimconfigs/vimrcs/plugins/colorschemes.vim
+' > ~/.vimrc
 
 read -p 'Do you want to enable Python support? (y/n): ' python_support
 read -p 'Do you want to enable Javascript support? (y/n): ' js_support
 read -p 'Do you want to enable frontend support? (y/n): ' frontend_support
-read -p 'Do you want to enable autocomplete support? (y/n): ' autocomplete_support
+read -p 'Do you want to enable YouCompleteMe (autocomplete) support? (y/n): ' autocomplete_support
 read -p 'Do you want to enable beta features? (y/n): ' beta_support
 
 if [ $python_support = "y" ]
 then
-    echo 'source ~/.vimconfigs/vimrcs/python.vim' >> ~/.vimrc
+    echo '" symbolic link for python suport' >> ~/.vimrc
+    echo 'source ~/.vimconfigs/vimrcs/plugins/python.vim' >> ~/.vimrc
     echo 'Python support enabled.'
 else
     echo 'Python support disabled.'
@@ -24,7 +35,8 @@ fi
 
 if [ $js_support = "y" ]
 then
-    echo 'source ~/.vimconfigs/vimrcs/js.vim' >> ~/.vimrc
+    echo '" symbolic link for javascript suport' >> ~/.vimrc
+    echo 'source ~/.vimconfigs/vimrcs/plugins/js.vim' >> ~/.vimrc
     echo 'Javascript support enabled.'
 else
     echo 'Javascript support disabled.'
@@ -32,7 +44,8 @@ fi
 
 if [ $frontend_support = "y" ]
 then
-    echo 'source ~/.vimconfigs/vimrcs/frontend.vim' >> ~/.vimrc
+    echo '" symbolic link for frontend suport' >> ~/.vimrc
+    echo 'source ~/.vimconfigs/vimrcs/plugins/frontend.vim' >> ~/.vimrc
     echo 'Frontend support enabled.'
 else
     echo 'Frontend support disabled.'
@@ -40,7 +53,8 @@ fi
 
 if [ $autocomplete_support = "y" ]
 then
-    echo 'source ~/.vimconfigs/vimrcs/autocomplete.vim' >> ~/.vimrc
+    echo '" symbolic link for youcomplete me suport' >> ~/.vimrc
+    echo 'source ~/.vimconfigs/vimrcs/plugins/autocomplete.vim' >> ~/.vimrc
     echo 'Autocomplete support enabled.'
 else
     echo 'Autocomplete support disabled.'
@@ -48,18 +62,22 @@ fi
 
 if [ $beta_support = "y" ]
 then
-    echo 'source ~/.vimconfigs/vimrcs/testing.vim' >> ~/.vimrc
+    echo '" symbolic link for beta suport' >> ~/.vimrc
+    echo 'source ~/.vimconfigs/vimrcs/basics/testing.vim' >> ~/.vimrc
     echo 'Beta support enabled.'
 else
     echo 'Beta support disabled.'
 fi
 
-echo 'call plug#end()
+echo '
+call plug#end()
 try
-source ~/.vimconfigs/my_configs.vim
+source ~/.vimconfigs/vimrcs/my_configs.vim
+source ~/.vimconfigs/vimrcs/my_plugins.vim
 catch
 endtry
-source ~/.vimconfigs/vimrcs/colors.vim' >> ~/.vimrc
+source ~/.vimconfigs/vimrcs/basics/colors.vim
+' >> ~/.vimrc
 
 # create undodir
 mkdir undodir
